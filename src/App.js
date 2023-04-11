b61c556fa3c07930c8c5bea35042fc2c45ba2c53
Rohith-Raju <rohithraju488@gmail.com>
2023-04-11 19:43:25 +0530

    working wasm
    Signed-off-by: Rohith-Raju <rohithraju488@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import createModule from './calculator.mjs'
+
 
 function App() {
+  const [add,setAdd] = useState()
+  useEffect(()=>{
+    createModule().then((module)=>{
+      setAdd(()=>module.cwrap("add","number",["number","number"]))
+    })
+  },[])
+  console.log(add)
+  if (!add){
+    return "loading wasm"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      add = {add(1,2)}
     </div>
   );
 }
